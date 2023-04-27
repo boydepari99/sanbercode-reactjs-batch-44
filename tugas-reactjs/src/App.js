bf1e562ef4417b6b8de9a8f04331354f20790a02
boydepari99 <boyadepari@gmail.com>
2023-04-28 01:20:34 +0800

    Update App.js

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
-import Tugas10 from './tugas10/tugas10';
-import Tugas11 from './tugas11/tugas11';
 import Tugas6 from './tugas6/tugas6';
 import Tugas7 from './tugas7/tugas7';
 import Tugas8 from './tugas8/tugas8';
 import Tugas9 from './tugas9/tugas9';
+import Tugas10 from './tugas10/tugas10';
+import Tugas11 from './tugas11/tugas11';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Tugas6 />
+      {/* <Tugas6 />
       <Tugas7 />
       <Tugas8 />
       <Tugas9 />
-      <Tugas10 />
+      <Tugas10 /> */}
       <Tugas11 />
     </div>
   );
