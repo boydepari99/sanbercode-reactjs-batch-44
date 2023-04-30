a6b49f58d8a6a5837c7ed617bedb249275398b07
boydepari99 <boyadepari@gmail.com>
2023-05-01 00:44:31 +0800

    update tugas 12

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -5,18 +5,25 @@ import Tugas8 from './tugas8/tugas8';
 import Tugas9 from './tugas9/tugas9';
 import Tugas10 from './tugas10/tugas10';
 import Tugas11 from './tugas11/tugas11';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './navbar';
 
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Tugas6 />
-      <Tugas7 />
-      <Tugas8 />
-      <Tugas9 />
-      <Tugas10 /> */}
-      <Tugas11 />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Tugas6 />}/>
+          <Route path='/tugas7' element={<Tugas7 />}/>
+          <Route path='/tugas8' element={<Tugas8 />}/>
+          <Route path='/tugas9' element={<Tugas9 />}/>
+          <Route path='/tugas10' element={<Tugas10 />}/>
+          <Route path='/tugas11' element={<Tugas11 />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
